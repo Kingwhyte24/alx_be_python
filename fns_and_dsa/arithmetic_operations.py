dd9b9f6fa915f5745c0dc2fb8ee291a5e7afb7bc
Kingwhyte24 <ohagbanusikingsley@gmail.com>
2025-01-16 10:00:49 +0100

    arithmetic_operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,1 +1,15 @@
-print('cover')
+def perform_operation(num1, num2, operation):
+    if operation == "add":
+        return num1 + num2
+    if operation == "subtract":
+        return num1 - num2
+    if operation == "multiply":
+        return num1 * num2
+    if operation == "divide":
+        if num2 == 0.0:
+            raise ValueError("Division by zero is not allowed.")
+        return num1 / num2
+    
+
+if __name__ == "__main__":
+    perform_operation()
