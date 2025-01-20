6773fc77ce88581eb70b18672b065d0952ac848e
Kingwhyte24 <ohagbanusikingsley@gmail.com>
2025-01-20 13:40:46 +0100

    test_simple_calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -17,7 +17,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(0, 0), 0)
         self.assertEqual(self.calc.add(2.5, 3.5), 6.0)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         """
         Test the subtract method with various cases.
         """
