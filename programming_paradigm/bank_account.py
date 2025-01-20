64c9bef44fc1e7e0118e54b1bf85656e2a8cedd4
Kingwhyte24 <ohagbanusikingsley@gmail.com>
2025-01-20 11:43:22 +0100

    programming_paradigm

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,22 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.account_balance = initial_balance
+
+    #deposit method
+    def deposit(self, amount):
+        if amount <= 0:
+            raise ValueError("Amount to deposit must be positive/greater than zero")
+        else:
+            self.account_balance += amount
+
+    def withdraw(self, amount):
+        if amount <= 0:
+            raise ValueError("Amount to withdraw must be positive/greater than zero")
+        if self.account_balance > amount:
+            self.account_balance -= amount
+            return True
+        else:
+            return False   
+        
+    def display(self):
+        print(f"Your account balance is {self.account_balance}")
